a852216c6a658e4303b5424d3184b0da029f6093
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-07-26 02:31:09 -0400

    Update buyer_dash.php

---
+++ buyer_dash.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<title>Buyer Dashboard</title>
+	</head>
+	<body>
+		<h1>Buyer Dashboard</h1>
+		<!-- Listing cards -->
+		<div id="listing_cards"></div>
+		<script src="get_listings.js"></script>
+	</body>
+</html>
