365a68ce0b5c9fc5bf1bd688c5129c5290ad2f9b
jamorr <Morris.Jack@outlook.com>
2023-07-27 17:10:01 -0400

    updated login frontend

--- login-auth.php
+++ login-auth.php
@@ -1,3 +1,29 @@
 <?php 
-header("Location: ./buyer_dash.php")
+require __DIR__."/common_database.php";
+$email = $_POST['email'];
+$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT); 
+
+
+$query = "SELECT * FROM Login WHERE email = $email;";
+$response = readFromTable($query, []);
+if (!$response) {
+    echo "User not found";
+} elseif ($response['pass'] === $pass) {
+    //successful login
+    $_SESSION['auth'] = true;
+    $_SESSION['email'] = $email;
+    if (!isset($_COOKIE['wishlist'])) {
+        setcookie("wishlist", [], 100000);
+    }
+    if ($response['new'] === true) {
+        $_SESSION['new'] = true;
+    }
+    else {
+        $_SESSION['new'] = false;
+ 
+    }
+    header("Location: ./buyer_dash.php");   
+} else {
+    echo "Incorrect password";
+}
 ?>
