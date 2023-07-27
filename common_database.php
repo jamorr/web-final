a336c7d73f9ad7b14d680ba6716e5f08545d8d17
jamorr <Morris.Jack@outlook.com>
2023-07-27 12:54:43 -0400

    added read function for mysql database

--- common_database.php
+++ common_database.php
@@ -1,7 +1,7 @@
 <?php
 function getEnvVars()
 {
-    $f = file("./.env");
+    $f = file(".env");
     if ($f === null) {
         return null;
     }
@@ -10,7 +10,6 @@ function getEnvVars()
 // Encryption function using OpenSSL
 function encryptData($data, $encryption_key, $cipher_method)
 {
-    // $cipher_method = 'aes-256-cbc';
     $iv_length = openssl_cipher_iv_length($cipher_method);
     $iv = openssl_random_pseudo_bytes($iv_length);
 
@@ -19,22 +18,40 @@ function encryptData($data, $encryption_key, $cipher_method)
 
     return $result;
 }
+function decryptData($encrypted_data, $encryption_key, $cipher_method)
+{
 
-// $encrypt is array who's first value is the encrytion type
-// and second is an assoc array whos keys are table columns to encrypt
+    $data = base64_decode($encrypted_data);
+    $iv_length = openssl_cipher_iv_length($cipher_method);
+    $iv = substr($data, 0, $iv_length);
+    $encrypted = substr($data, $iv_length);
+
+    $decrypted = openssl_decrypt($encrypted, $cipher_method, $encryption_key, OPENSSL_RAW_DATA, $iv);
+    return $decrypted;
+}
+
+function initConnection($table_vars)
+{
+    file_put_contents("./db.log", print_r(getEnvVars(), true));
+    $conn = new mysqli($table_vars[0], $table_vars[1], $table_vars[1], $table_vars[1]);
+    if ($conn->connect_error) {
+        echo "Could not connect to server\n";
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+    // $encrypt is array who's first value is the encrytion type
+    // and second is an assoc array whos keys are table columns to encrypt
 function writeToTable(string $table, array $encrypt, $data)
 {
     try {
-        // $table_vars = getEnvVars();
         $table_vars = null;
         if ($table_vars === null) {
             $table_vars = ["localhost", "jmorris116", "batman"];
         }
-        $conn = new mysqli($table_vars[0], $table_vars[1], $table_vars[1], $table_vars[1]);
-        if ($conn->connect_error) {
-            echo "Could not connect to server\n";
-            die("Connection failed: " . $conn->connect_error);
-        }
+
+        $conn = initConnection($table_vars);
         $decoded = json_decode($data);
         $key_str = "";
         $val_str = "";
@@ -61,5 +78,41 @@ function writeToTable(string $table, array $encrypt, $data)
     }
 }
 
+function readFromTable(string $query, array $encrypted)
+{
+    try {
+        file_put_contents("./db.log", print_r(getEnvVars(), true));
+    
+        $table_vars = getEnvVars();
+        $table_vars = null;
+        if ($table_vars === null) {
+            $table_vars = ["localhost", "jmorris116", "batman"];
+        }
+        $conn = new mysqli($table_vars[0], $table_vars[1], $table_vars[1], $table_vars[1]);
+        if ($conn->connect_error) {
+            echo "Could not connect to server\n";
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $data = $conn->query($query);
+        $output = [];
+        while ($row = $data->fetch_assoc()) {
+            foreach ($encrypted as $key => $value) {
+                $row[$value] = decryptData($row[$value], $table_vars[2], $encrypted[0]);
+            }
+            array_push($output, $row);
+        }
+        echo json_encode($output);
+        $conn->close();
+    } catch (Exception $e) {
+        // Log the error (optional)
+        error_log($e->getMessage());
+
+        // Return an error response to the front-end
+        http_response_code(500); // Set the appropriate HTTP status code for the error
+        echo json_encode(array('error' => $e->getMessage()));
+    }
+
+}
 
 ?>
