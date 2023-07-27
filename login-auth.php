2a238fd2c1bf19c6714f4ffd008cb17704501ef7
jamorr <Morris.Jack@outlook.com>
2023-07-27 14:28:42 -0400

    Added initial version of authorization and login cookie

--- login-auth.php
+++ login-auth.php
@@ -1,6 +1,29 @@
 <?php 
 require __DIR__."/common_database.php";
-readFromTable()
+$email = $_POST['email'];
+$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT); 
+
+
+$query = "SELECT * FROM Login WHERE email = $email;";
+$response = readFromTable($query, []);
+if ($response === "") {
+    echo "User not found";
+} elseif ($response['pass'] === $pass) {
+    $_SESSION['auth'] = true;
+    $_SESSION['email'] = $email;
+    if (!isset($_COOKIE['wishlist'])) {
+        setcookie("wishlist", [], 100000);
+    }
+    if ($response['new'] === true) {
+        $_SESSION['new'] = true;
+    }
+    else {
+        $_SESSION['new'] = false;
+    }
+} else {
+    echo "Incorrect password";
+}
+
 
 header("Location: ./buyer_dash.php");
 
