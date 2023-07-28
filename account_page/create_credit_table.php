bdea171312e2f5771581154ce78af73996568a33
jamorr <Morris.Jack@outlook.com>
2023-07-28 18:49:11 -0400

    apewo

--- account_page/create_credit_table.php
+++ account_page/create_credit_table.php
@@ -6,19 +6,17 @@ $conn = initConnection(["localhost", "jmorris116", "batman"]);
 $sql = "CREATE TABLE Credit (
 	email VARCHAR(60) PRIMARY KEY,
   cc_name VARCHAR(50) NOT NULL,
-  cc_num VARCHAR(16) NOT NULL,
+  cc_num VARCHAR(50) NOT NULL,
   cc_exp_MM INT NOT NULL,
   cc_exp_YY INT NOT NULL,
   cc_addr VARCHAR(50) NOT NULL,
   cc_billing_addr VARCHAR(50) NOT NULL,
   cc_phone VARCHAR(18) NOT NULL );";
-
 if ($conn->query($sql) === true) {
     echo "Table Listings created successfully<br>";
 } else {
     echo "Error creating table: " . $conn->error . "<br>";
 }
-
-$conn->close();
+$conn->close()
 
 ?>
