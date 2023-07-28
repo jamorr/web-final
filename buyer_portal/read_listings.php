01aebe158090baf2d90983c74001f2c0ff5cf2b5
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-07-28 15:09:12 -0400

    Update read_listings.php

--- buyer_portal/read_listings.php
+++ buyer_portal/read_listings.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__."../common_database.php";
+require "../common_database.php";
 // Get the offset and the number of rows to query.
 $offset = $_GET['offset'];
 $limit = $_GET['limit'];
@@ -27,12 +27,15 @@ OR city LIKE '%{$query}%'
 OR state_abbrev LIKE '%{$query}%' 
 OR zip LIKE '%{$query}%') ";
 
+$column_names = ["price", "bedrooms", "bathrooms", "floor_area", "lot_area"];
+
 // add filters 
 for ($i=0; $i < count($element_ids); $i++) { 
     $key = $element_ids[$i];
     $symbol = ($i % 2 === 0)?">=":"<=";
+	$col_name = (int) $i / 2;
     if (isset($_GET[$key])) { 
-        $sql.= "AND $key $symbol '$_GET[$key]'";
+        $sql.= "AND $column_names[$col_name] $symbol '$_GET[$key]' ";
     }
 }
 // include offset and limit
