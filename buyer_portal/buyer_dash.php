aee4cf604bc4b04c1e6f46e2c670a1116dd14a71
jamorr <Morris.Jack@outlook.com>
2023-07-29 07:40:47 -0400

    updated files to work on jack's codd

--- buyer_portal/buyer_dash.php
+++ buyer_portal/buyer_dash.php
@@ -1,68 +1,69 @@
 <?php
 // Checking if the account is authorized.
 session_start();
-if (!isset($_SESSION['auth']) || !$_SESSION['auth'])
-	header("Location: ../index.html");
+if (!isset($_SESSION['auth']) || !$_SESSION['auth']) {
+    header("Location: ../index.html");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<link rel="stylesheet" href="styles/style.css" />
-		<script src="scripts/wishlist.js"></script>
-		<title>nestXchange • Dashboard</title>
-	</head>
-	<body>
-		<!-- Header -->
-		<header class="main-header">
-			<h1>nestXchange</h1>
-			<a href="../account_page/account.">Account settings</a> •
-			<a href="">Wish list</a>
-		</header>
-		
-		<!-- Search elements -->
-		<div id="search_container">
-			<input type="text" id="query" />
-			<button id="search">Search</button>
-			<button type="button" class="accordion-button" onclick="showDropdown();">
-				Advanced Filters
-			</button>
-			<div class="adv-search" style="display: none" id="dropdown">
-				<label for="min_price">Price:</label>
-				<input type="text" id="min_price" placeholder="min" />
-				<input type="text" id="max_price" placeholder="max" /><br />
-				<label for="min_bed">Beds:</label>
-				<input type="text" id="min_bed" placeholder="min" />
-				<input type="text" id="max_bed" placeholder="max" /><br />
-				<label for="min_bath">Baths:</label>
-				<input type="text" id="min_bath" placeholder="min" />
-				<input type="text" id="max_bath" placeholder="max" /><br />
-				<label for="min_floor_area">Floor Area (sqft):</label>
-				<input type="text" id="min_floor_area" placeholder="min" />
-				<input type="text" id="max_floor_area" placeholder="max" /><br />
-				<label for="min_lot_area">Lot Area (sqft):</label>
-				<input type="text" id="min_lot_area" placeholder="min" />
-				<input type="text" id="max_lot_area" placeholder="max" /><br />
-			</div>
-		</div>
-		
-		<!-- Listing cards -->
-		<div id="listing_cards"></div>
-		<script src="scripts/get_listings.js"></script>
-		
-		<!-- Script for drop-down advanced search -->
-		<script>
-		function showDropdown() {
-			const drop = document.getElementById("dropdown");
-			if (drop.style.display === "inline-block") {
-			drop.style.display = "none";
-			} else {
-			drop.style.display = "inline-block";
-			}
-		}
-		</script>
-		
-		<footer>&copy; 2023 nestXchange</footer>
-	</body>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="styles/style.css" />
+        <script src="scripts/wishlist.js"></script>
+        <title>nestXchange • Dashboard</title>
+    </head>
+    <body>
+        <!-- Header -->
+        <header class="main-header">
+            <h1>nestXchange</h1>
+            <a href="../account_page/account.php">Account settings</a> •
+            <a href="">Wish list</a>
+        </header>
+        
+        <!-- Search elements -->
+        <div id="search_container">
+            <input type="text" id="query" />
+            <button id="search">Search</button>
+            <button type="button" class="accordion-button" onclick="showDropdown();">
+                Advanced Filters
+            </button>
+            <div class="adv-search" style="display: none" id="dropdown">
+                <label for="min_price">Price:</label>
+                <input type="text" id="min_price" placeholder="min" />
+                <input type="text" id="max_price" placeholder="max" /><br />
+                <label for="min_bed">Beds:</label>
+                <input type="text" id="min_bed" placeholder="min" />
+                <input type="text" id="max_bed" placeholder="max" /><br />
+                <label for="min_bath">Baths:</label>
+                <input type="text" id="min_bath" placeholder="min" />
+                <input type="text" id="max_bath" placeholder="max" /><br />
+                <label for="min_floor_area">Floor Area (sqft):</label>
+                <input type="text" id="min_floor_area" placeholder="min" />
+                <input type="text" id="max_floor_area" placeholder="max" /><br />
+                <label for="min_lot_area">Lot Area (sqft):</label>
+                <input type="text" id="min_lot_area" placeholder="min" />
+                <input type="text" id="max_lot_area" placeholder="max" /><br />
+            </div>
+        </div>
+        
+        <!-- Listing cards -->
+        <div id="listing_cards"></div>
+        <script src="scripts/get_listings.js"></script>
+        
+        <!-- Script for drop-down advanced search -->
+        <script>
+        function showDropdown() {
+            const drop = document.getElementById("dropdown");
+            if (drop.style.display === "inline-block") {
+            drop.style.display = "none";
+            } else {
+            drop.style.display = "inline-block";
+            }
+        }
+        </script>
+        
+        <footer>&copy; 2023 nestXchange</footer>
+    </body>
 </html>
